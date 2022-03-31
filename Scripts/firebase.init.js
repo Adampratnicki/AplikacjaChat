141728b87058664701fd34e4c1f917b2883ff488
Adampratnicki <87644400+Adampratnicki@users.noreply.github.com>
2022-03-31 08:37:02 +0200

    Add files via upload

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,12 @@
+﻿// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAW3vEPslIUNdHxkzh8QI1zgljX3qV1Iss",
+    authDomain: "chat-app-48997.firebaseapp.com",
+    databaseURL: "https://chat-app-48997-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chat-app-48997",
+    storageBucket: "chat-app-48997.appspot.com",
+    messagingSenderId: "301846460247",
+    appId: "1:301846460247:web:0f67e252c8f02ed0eeb012"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,38 @@
+﻿// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAAmmPANHkbigecLk-7P8LAZy2jS_0yNqQ",
+    authDomain: "chatek-d34e8.firebaseapp.com",
+    projectId: "chatek-d34e8",
+    storageBucket: "chatek-d34e8.appspot.com",
+    messagingSenderId: "579541472743",
+    appId: "1:579541472743:web:ffd9619b57c46b4dc25942"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'You have new message';
+    const notificationOptions = {
+        body: payload.data.message,
+        icon: payload.data.icon
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
+
+
